bc2fcacb04c6d156976dce5ff494ef98fd0e62c9
yingbbang <yingbbang@naver.com>
2025-07-11 11:39:41 +0900

    Update MethodTest.java

--- ch04/src/sub2/MethodTest.java
+++ ch04/src/sub2/MethodTest.java
@@ -15,10 +15,12 @@ public class MethodTest { // Java 파일은 class 구조체로서 Method로 구�
 		int y1 = f(1); // 인자값 1을 f메서드의 매개변수 x에 대입
 		int y2 = f(2); // 인자값 2을 f메서드의 매개변수 x에 대입
 		int y3 = f(3); // 인자값 3을 f메서드의 매개변수 x에 대입
+		int y4 = f(4);
 		
 		System.out.println("y1 : " + y1);
 		System.out.println("y2 : " + y2);
 		System.out.println("y3 : " + y3);
+		System.out.println("y4 : " + y4);
 	
 		// 지역변수와 메서드 호출
 		int total1 = sum(1,10);
