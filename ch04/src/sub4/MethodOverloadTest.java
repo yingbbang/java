68b392eba8404c4f6cba2728775a417d1039b033
code00morning <code00morning@naver.com>
2025-07-11 11:35:25 +0900

    오버로드 메서드 수정

--- ch04/src/sub4/MethodOverloadTest.java
+++ ch04/src/sub4/MethodOverloadTest.java
@@ -12,11 +12,13 @@ public class MethodOverloadTest {
 		int r2 = add(2, 3);
 		double r3 = add(1.1, 3.14);
 		String r4 = add("Hello", "Java");
+		String r5 = add("number_", 1);
 		
 		System.out.println("r1 : " + r1);
 		System.out.println("r2 : " + r2);
 		System.out.println("r3 : " + r3);
 		System.out.println("r4 : " + r4);
+		System.out.println("r4 : " + r5);
 		
 	
 	}
@@ -37,6 +39,10 @@ public class MethodOverloadTest {
 	
 	public static String add(String a, String b) {
 		return a + b;
+	}
+	
+	public static String add(String a, int b) {		
+		return a + b;		
 	}
 	
 	
